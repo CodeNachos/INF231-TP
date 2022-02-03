9c69f9d8b3eb01e02ee803ac00400fc83ffd0b9c
CodeNachos <62766805+CodeNachos@users.noreply.github.com>
2022-02-03 16:02:58 +0100

    Update tp2.ml

--- TP2/tp2.ml
+++ TP2/tp2.ml
@@ -26,7 +26,7 @@ monEt false true ;; (*This returns false because it checks first the left object
 (*We can conclude that when we call a function, it first evaluates the parameters that we putted before entering the function so if there is an error in one of the paramater
   ,which is our case with monEt (b <> 0) (a mod b=0) because a mod b raises an error, OCaml will just say there is an error and won't execute the function *)
 
-(*
+(* 
  EXERCICE 2.6
  Q1.
 *)
@@ -34,24 +34,24 @@ monEt false true ;; (*This returns false because it checks first the left object
 (* DEFINING FUNCTIONS *)
 
 let max2 (x:int) (y:int): int =
-        (abs(x-y)+(x+y))/2
+	(abs(x-y)+(x+y))/2
 ;;
 
 let min2 (x:int) (y:int): int =
-        if max2 x y = x then y else x
+	if max2 x y = x then y else x
 ;;
 
 let max4 (a:int)(b:int) (c:int) (d:int): int =
-        max2 (max2 a b) (max2 c d)
+	max2 (max2 a b) (max2 c d)
 ;;
 
 let min4 (a:int) (b:int) (c:int) (d:int): int =
-        min2 (min2 a b) (min2 c d)
+	min2 (min2 a b) (min2 c d)
 ;;
 
 let moyol (a:int) (b:int) (c:int) (d:int): float =
-        let max = max4 a b c d and min = min4 a b c d in
-                float_of_int((a+b+c+d)-(max+min)) /. 2.;;
+	let max = max4 a b c d and min = min4 a b c d in
+		float_of_int((a+b+c+d)-(max+min)) /. 2.;;
 ;;
 
 (* Q2. *)
@@ -93,4 +93,100 @@ moyol 0 0 0 0;;
 moyol 1 0 0 1;;
 moyol 20 5 7 48;;
 moyol (-1) 0 (-1) 0;;
-moyol (-3) (-56) (-14) (-10);;
+moyol (-3) (-56) (-14) (-10);; 
+
+(* 
+ Q3.
+ tracing a test 
+*)
+
+#trace moyol;;
+#trace max2;;
+#trace min2;;
+#trace max4;;
+#trace min4;;
+
+moyol 20 5 7 48;;
+
+#untrace moyol;;
+#untrace max2;;
+#untrace min2;;
+#untrace max4;;
+#untrace min4;;
+
+(* 
+Q4.
+identing (moyol 20 5 7 48) trace:
+
+moyol <-- 20
+moyol --> <fun>
+moyol* <-- 5
+moyol* --> <fun>
+moyol** <-- 7
+moyol** --> <fun>
+moyol*** <-- 48
+
+	max4 <-- 20
+	max4 --> <fun>
+	max4* <-- 5
+	max4* --> <fun>
+	max4** <-- 7
+	max4** --> <fun>
+	max4*** <-- 48
+	
+		max2 <-- 7
+		max2 --> <fun>
+		max2* <-- 48
+		max2* --> 48
+		
+		max2 <-- 20
+		max2 --> <fun>
+		max2* <-- 5
+		max2* --> 20
+		
+		max2 <-- 20
+		max2 --> <fun>
+		max2* <-- 48
+		max2* --> 48
+		
+	max4*** --> 48
+	
+	min4 <-- 20
+	min4 --> <fun>
+	min4* <-- 5
+	min4* --> <fun>
+	min4** <-- 7
+	min4** --> <fun>
+	min4*** <-- 48
+	
+		min2 <-- 7
+		min2 --> <fun>
+		min2* <-- 48
+			max2 <-- 7
+			max2 --> <fun>
+			max2* <-- 48
+			max2* --> 48
+		min2* --> 7
+		
+		min2 <-- 20
+		min2 --> <fun>
+		min2* <-- 5
+			max2 <-- 20
+			max2 --> <fun>
+			max2* <-- 5
+			max2* --> 20
+		min2* --> 5
+		
+		min2 <-- 5
+		min2 --> <fun>
+		min2* <-- 7
+			max2 <-- 5
+			max2 --> <fun>
+			max2* <-- 7
+			max2* --> 7
+		min2* --> 5
+		
+	min4*** --> 5
+moyol*** --> 13.5
+*)
+
