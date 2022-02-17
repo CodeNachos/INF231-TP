670446c0fed2d937ac5471b957f2a1c88a91ebc0
CodeNachos <62766805+CodeNachos@users.noreply.github.com>
2022-02-17 13:27:52 +0100

    Create tp4.ml

---
+++ TP4/tp4.ml
@@ -0,0 +1,7 @@
+(* -----------------------------------------------------------------------
+   inf201-ABDELKADER-MASCARENHAS-DOKTORCIK-TP3.ml : cr exercices TP no4
+   Youssef ABDELKADER <Youssef.Abdelkader@etu.univ-grenoble-alpes.fr>  \ 
+   Rafael MASCARENHAS <Rafael.Mascarenhas@etu.univ-grenoble-alpes.fr>   > Groupe Ocaml_best_camel
+   Maxence DOKTORCIK <Maxence.Doktorcik@etu.univ-grenoble-alpes.fr>    / 
+----------------------------------------------------------------------- *)
+
