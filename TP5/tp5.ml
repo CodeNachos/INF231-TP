e248d8db00e83e5b9043cd2bebbe7da2f0eb5f64
TheAmazingSui <95222394+TheAmazingSui@users.noreply.github.com>
2022-03-03 15:29:22 +0100

    Update tp5.ml

--- TP5/tp5.ml
+++ TP5/tp5.ml
@@ -4,8 +4,130 @@
    Rafael MASCARENHAS <Rafael.Mascarenhas@etu.univ-grenoble-alpes.fr>   > Groupe Ocaml_best_camel
    Maxence DOKTORCIK <Maxence.Doktorcik@etu.univ-grenoble-alpes.fr>    / 
 ----------------------------------------------------------------------- *)
+(*3.1 Factorielle*)
+
+(*Q.1*)
+
+let rec factorielle (p:int):int = match p with
+  |0 -> 1
+  |_ -> p * factorielle (p-1);;
+
+(*Q.2*)
+
+(*Test with 5*)
+(factorielle) 5;;
+
+(*Test with 4*)
+(factorielle) 4;;
+
+(*Tracing of the function:
+  factorielle <-- 5
+   factorielle <-- 4
+      factorielle <-- 3
+         factorielle <-- 2
+            factorielle <-- 1
+               factorielle <-- 0
+               factorielle --> 1
+            factorielle --> 1
+         factorielle --> 2
+      factorielle --> 6
+   factorielle --> 24
+  factorielle --> 120
+  - : int = 120*)
+
+(*Q.3*)
+
+(*(factorielle) (-1);;*)
+
+(*We get an error: Stack overflow during evaluation (looping recursion?), which 
+  means that the recursion never ends. This makes sense, since we keep removing -1 from the initial
+  parameter and the recursion ends once eventually the difference reaches 0. If we input
+  a negative number in the first place, the parameter never reaches 0. The recursion 
+  goes on forever.*)
+
+(*Tracing of (factorielle) (-1)*)
+(*        
+# (factorielle) (-1);;
+factorielle <-- -1
+factorielle <-- -2
+factorielle <-- -3
+factorielle <-- -4
+factorielle <-- -5
+factorielle <-- -6
+factorielle <-- -7
+factorielle <-- -8
+factorielle <-- -9
+factorielle <-- -10
+factorielle <-- -11
+factorielle <-- -12
+factorielle <-- -13
+factorielle <-- -14
+factorielle <-- -15
+factorielle <-- -16
+factorielle <-- -17
+factorielle <-- -18
+factorielle <-- -19
+factorielle <-- -20
+factorielle <-- -21
+factorielle <-- -22
+factorielle <-- -23
+factorielle <-- -24
+factorielle <-- -25
+factorielle <-- -26
+factorielle <-- -27
+factorielle <-- -28
+factorielle <-- -29
+factorielle <-- -30
+Stack overflow during evaluation (looping recursion?).#
+As we can see, the function keeps going on forever. *)
+
+(*Q.4*)
+
+let factorielle2 (p:int):int = match p with
+  |0 -> 1
+  |_ -> p * factorielle2 (p-1);;
+
+(*When we forget the 'rec', we get the following error:
+  Error: Unbound value factorielle.
+  We also get the same error when defining the function itself. (Additionaly, Ocaml suggests us to 
+  add 'rec' : 
+  Hint: Did you mean factorielle?
+  Hint: If this is a recursive definition,
+  you should add the 'rec' keyword on line 1). 
+  Since the function is not recursive, Ocaml interprets the 'factorielle' that is in 
+  the body of the function as an undefined variable.*)
+
+(*Q.5*)
+
+let rec factorielle3 (p:int):int = match p with
+  |p -> p * factorielle (p-1)
+  |0 -> 1;; 
+
+(*When defining the function, we get the following message:
+  Warning 11 [redundant-case]: this match case is unused.*)
+
+(*Test of factorille3:*)
+(*(factorielle3 ) 5;; *)
+
+(*When we test the function (with whatever value), we get the following error:
+  Stack overflow during evaluation (looping recursion?).*)
+
+(*Which makes sense. The first error we get when defining the function tells us that
+  the second case in pattern matching is "not used", which is true since we'll be always
+  matching the parameter p with itself, and p cannot be different than itself at any given moment,
+  so the other case is useless. When we give it a parameter, the code doesn't work
+  since the recursion doesn't end (since we're continuously matching p with itself), that's
+  why it raises an error.*)
+
+(*Q.6*)
+
+(*We suppose that all constraints are respected*)
+let rec fact3 (p:int):int = if p = 0 then 1 else p * fact3 (p-1);;
+(fact3) 6;;
 
 
+
+-----
 (*
    Exercice 3.3.1
    Q1.
