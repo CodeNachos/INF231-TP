6ccf4ce6c5823f872758e09a0d32bbb04c16c280
CodeNachos <62766805+CodeNachos@users.noreply.github.com>
2022-03-10 16:12:12 +0100

    Update tp6.ml

--- TP6/tp6.ml
+++ TP6/tp6.ml
@@ -1,5 +1,5 @@
 (* -----------------------------------------------------------------------
-   inf201-ABDELKADER-MASCARENHAS-DOKTORCIK-TP3.ml : cr exercices TP no5
+   inf201-ABDELKADER-MASCARENHAS-DOKTORCIK-TP3.ml : cr exercices TP no6
    Youssef ABDELKADER <Youssef.Abdelkader@etu.univ-grenoble-alpes.fr>  \ 
    Rafael MASCARENHAS <Rafael.Mascarenhas@etu.univ-grenoble-alpes.fr>   > Groupe Ocaml_best_camel
    Maxence DOKTORCIK <Maxence.Doktorcik@etu.univ-grenoble-alpes.fr>    / 
