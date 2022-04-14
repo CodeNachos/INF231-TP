d3ee7c95f1d0b3ec4a35d50b42e7c37281a6e7a9
TheAmazingSui <95222394+TheAmazingSui@users.noreply.github.com>
2022-04-14 16:05:33 +0200

    Update tp11.ml

--- TP11/tp11.ml
+++ TP11/tp11.ml
@@ -138,3 +138,96 @@ arbreVchaine_op string_of_int ex12 ;;
 
 
 (* Q9. *)
+
+
+-------
+
+(*6.2 Sommes *)
+type binary_tree =
+  | Empty
+  | Node of int * binary_tree * binary_tree
+
+(*Q.1*)
+
+let rec somme_arbre (t:binary_tree) = match t with
+  |Empty -> 0
+  |Node(a,lt,rt) -> a + somme_arbre lt + somme_arbre rt;; 
+let bt1 =
+  Node (100,
+        Node (30,Empty,Empty),
+        Node (74,Empty,Empty)
+       )
+
+
+(*Q.2*)
+let ex1 = Node(15,Node (7,Empty,Node(13,Empty,Empty)),Node(33,Empty,Empty));;
+let ex2 = Node(15,Node (7,Node(-8,Empty,Empty),Empty),Node(-6,Empty,Empty));;
+(somme_arbre) ex1;;
+(somme_arbre) ex2;;
+
+(*Q.3*)
+(*
+somme_arbre <--Node (15, Node (7, Empty, Node (13, Empty, Empty)),Node (33, Empty, Empty))
+  somme_arbre <-- Node (33, Empty, Empty)
+    somme_arbre <-- Empty
+    somme_arbre --> 0
+    somme_arbre <-- Empty
+    somme_arbre --> 0
+  somme_arbre --> 33
+  somme_arbre <-- Node (7, Empty, Node (13, Empty, Empty))
+    somme_arbre <-- Node (13, Empty, Empty)
+      somme_arbre <-- Empty
+      somme_arbre --> 0
+      somme_arbre <-- Empty
+      somme_arbre --> 0
+    somme_arbre --> 13
+    somme_arbre <-- Empty
+    somme_arbre --> 0
+  somme_arbre --> 20
+somme_arbre --> 68
+- : int = 68
+*)
+
+(*Q.4*)
+let rec somme_pos_neg_0 (t:binary_tree):int*int*int  = match t with
+  |Empty -> (0,0,0)
+  |Node(a,lt,rt) -> let (pos1,neg1,nul1) = somme_pos_neg_0 lt and (pos2,neg2,nul2) = somme_pos_neg_0 rt in 
+    if a>0 then (a+pos1+pos2, neg2+neg1,0) else if a<0 then (pos1+pos2, neg1 + neg2 + a,0) else (pos1+pos2,neg1+neg2,1+a);;     
+(somme_pos_neg_0) ex1;;
+(somme_pos_neg_0) ex2;;
+
+
+(*6.3 Arbres symétriques*)
+
+(*Q.1*)
+
+(*
+                                                  1
+                                                /  \
+                                               4    2
+                                              / \    \
+                                             7   5    3
+                                                /
+                                               6
+*)
+
+(*Q.2*)
+let rec sym_arbe (t:binary_tree):binary_tree = match t with
+  |Empty -> Empty
+  |Node(a,lt,rt) -> Node(a, sym_arbe rt, sym_arbe lt);;
+
+(sym_arbe) ex1;;
+
+(*Q.3*)
+let ex3 = sym_arbe ex1;;
+let sont_sym (t1:binary_tree) (t2:binary_tree):bool = if t1 = sym_arbe t2 then true else false;;
+(sont_sym) ex3 ex1;;
+
+(*Q.4*)
+let sont_sym2 (t1:binary_tree) (t2:binary_tree) :bool = let rec transform_arbre_seq (t:binary_tree):int list = match t with
+    |Empty -> []
+    |Node(a,lt,rt) -> (a:: transform_arbre_seq lt)@(transform_arbre_seq rt)
+  in transform_arbre_seq t2 = transform_arbre_seq (sym_arbe t1) ;; 
+
+(sont_sym2) ex1 ex3;;
+
